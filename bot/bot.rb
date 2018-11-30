e3df8ac99905e3f973dc184ace328598f84cad23
Dmitry Chernyatiev <dchernyatiev@taxdome.com>
2018-11-30 20:54:22 +0400

    Bootstrap sinatra

---
+++ bot/bot.rb
@@ -0,0 +1,13 @@
+# bot.rb
+require 'sinatra'
+
+get '/' do
+  data = { message: 'Hello!' }
+  content_type :json
+  data.to_json
+end
+
+error JSON::ParserError do
+  status 422
+  { error: 'Invalid json' }
+end
