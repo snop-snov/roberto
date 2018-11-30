8b7b7386d0703c159f0b67bd9dda6650caf3df09
Dmitry Chernyatiev <dchernyatiev@taxdome.com>
2018-11-30 22:37:16 +0400

    Add post

--- bot.rb
+++ bot.rb
@@ -7,6 +7,12 @@ get '/' do
   data.to_json
 end
 
+post '/messages' do
+  data = { message: 'Hello!' }
+  content_type :json
+  data.to_json
+end
+
 error JSON::ParserError do
   status 422
   { error: 'Invalid json' }
